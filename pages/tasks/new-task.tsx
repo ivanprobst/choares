2e18634b498fafe7dfb70fc5ced57bba8a7673c1
Ivan Probst <ivan.probst@gmail.com>
2022-04-22 17:55:26 +0200

    fixed comp name casing

--- pages/tasks/new-task.tsx
+++ pages/tasks/new-task.tsx
@@ -3,7 +3,7 @@ import type { NextPage } from "next";
 import { useRouter } from "next/router";
 import toast from "react-hot-toast";
 
-import Layout from "../../components/layout";
+import Layout from "../../components/Layout";
 import styles from "../../styles/Home.module.css";
 import useLocale from "../../state/useLocale";
 import { APIResponseType, TaskDBType } from "../../utils/types";
